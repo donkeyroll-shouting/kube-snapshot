602e6926b3dfde24d1a0a3bcc894a397782c0392
donkeyroll <shouting.dev@gmail.com>
2024-10-08 20:53:32 -0700

    fix(gitignore): ignore the wrong directory
    * Add cmd/ksnap directory which implement the root CLI and install kubectl flags.

---
+++ cmd/ksnap/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
+	"k8s.io/cli-runtime/pkg/genericclioptions"
+)
+
+const FlagNameKubeconfig = "kubeconfig"
+
+var rootCmd = &cobra.Command{
+	Use:   "ksnap",
+	Short: "A Kubernetes snapshot tool",
+	Long:  `ksnap is a CLI tool for taking snapshots of Kubernetes clusters.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Hello, world!")
+	},
+}
+
+func main() {
+	// Add k8s generic CLI flags.
+	configFlags := genericclioptions.NewConfigFlags(true)
+	configFlags.AddFlags(rootCmd.Flags())
+
+	// Loop through the flags and mark them as hidden unless they are "kubeconfig".
+	rootCmd.Flags().VisitAll(func(flag *pflag.Flag) {
+		if flag.Name != FlagNameKubeconfig {
+			flag.Hidden = true
+		}
+	})
+
+	if err := rootCmd.Execute(); err != nil {
+		os.Exit(1)
+	}
+}
